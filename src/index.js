51987153782eb6092b9bb002d33953b3598e2102
Andrej Berezni <berezniandrej@gmail.com>
2023-02-09 11:30:44 +0000

    first version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const URL = 'https://api.quotable.io/random';
+
+function App() {
+  const [text, setText] = useState({});
+
+  function getQuote(){
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => setText(data))
+  }
+  
+  useEffect(() => {
+    getQuote();
+  }, []);
+  
+ return (
+   <div id='quote-box'>
+    <p id='text'>{text.content}</p>
+    <h3 id='author'>{text.author}</h3>
+    <div id='button-box'>
+      <button id='new-quote' onClick={getQuote}>New Quote</button>
+      <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${text.content} - ${text.author}`} target="_blank"><img src='https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-twitter-512.png'/>Tweet this quote</a>
+     </div>
+    </div>
+   )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +36,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
